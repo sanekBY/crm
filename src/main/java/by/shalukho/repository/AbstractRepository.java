692e9c3b6bcd92465557138e394bd707c13938e0
sanekBY <sashqua.minsk@gmail.com>
2018-08-16 19:04:53 +0300

    small fix

--- src/main/java/by/shalukho/repository/AbstractRepository.java
+++ src/main/java/by/shalukho/repository/AbstractRepository.java
@@ -3,6 +3,6 @@ package by.shalukho.repository;
 import by.shalukho.dbo.AbstractEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface еAbstractRepository<Entity extends AbstractEntity, Long> extends JpaRepository<Entity, Long> {
-
+public interface AbstractRepository<Entity extends AbstractEntity, Long> extends JpaRepository<Entity, Long> {
+    Entity findIsActive();
 }
